8ef6d20ac5d58bfa6785ab22ccfb5331e43a702f
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2025-03-21 08:25:39 -0500

    Update GestorUsuarios.java

--- src/main/java/co/edu/uniquindio/gestorcontactos/modelo/GestorUsuarios.java
+++ src/main/java/co/edu/uniquindio/gestorcontactos/modelo/GestorUsuarios.java
@@ -10,6 +10,22 @@ public class GestorUsuarios {
     }
 
     public void crearContacto(String nombre, String apellido, String telefono, LocalDate fechaCumpleanios, String correo) throws Exception{
+        if(nombre == null || nombre.isEmpty()){
+            throw new Exception("El nombre es obligatorio");
+        }
+
+        if(apellido == null || apellido.isEmpty()){
+            throw new Exception("La apellido es obligatoria");
+        }
+
+        if(telefono == null || telefono.isEmpty()){
+            throw new Exception("El telefono es obligatorio");
+        }
+
+        if(correo == null || correo.isEmpty()){
+            throw new Exception("El correo es obligatoria");
+        }
+        
         Usuario usuarioBuscado = buscarUsuarioNombre(nombre);
         if (usuarioBuscado == null){
             usuarioBuscado = buscarUsuarioTelefono(telefono);
