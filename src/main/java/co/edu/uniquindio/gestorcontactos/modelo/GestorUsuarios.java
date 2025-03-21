a688e7ac8e872a55cdfd02805a5e00cbe4f63d68
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2025-03-21 08:12:55 -0500

    Metodos de Gestion de Usuarios

--- src/main/java/co/edu/uniquindio/gestorcontactos/modelo/GestorUsuarios.java
+++ src/main/java/co/edu/uniquindio/gestorcontactos/modelo/GestorUsuarios.java
@@ -9,15 +9,59 @@ public class GestorUsuarios {
         listaUsuarios = new LinkedList<>();
     }
 
-    public void crearContacto(String nombre, String apellido, String telefono, LocalDate fechaCumpleanios, String correo){
-        Usuario usuario = new Usuario(nombre,  apellido, telefono, fechaCumpleanios, correo);
-        listaUsuarios.add(usuario);
+    public void crearContacto(String nombre, String apellido, String telefono, LocalDate fechaCumpleanios, String correo) throws Exception{
+        Usuario usuarioBuscado = buscarUsuarioNombre(nombre);
+        if (usuarioBuscado == null){
+            usuarioBuscado = buscarUsuarioTelefono(telefono);
+        }
+
+        if(usuarioBuscado!=null){
+            throw new Exception("Ya existe un usuario con el mismo ID");
+        }else{
+            Usuario usuario = new Usuario(nombre, apellido, telefono, fechaCumpleanios, correo);
+            listaUsuarios.add(usuario);
+        }
     }
 
-    public void buscarUsuario(String nombre,String telefono){
+    public Usuario buscarUsuarioTelefono(String telefono){
         return listaUsuarios.stream()
-                .filter(billetera -> billetera.getNumero().equals(numero))
+                .filter(usuario -> usuario.getTelefono().equals(telefono))
                 .findFirst()
                 .orElse(null);
+    }
+
+    public Usuario buscarUsuarioNombre(String nombre){
+        return listaUsuarios.stream()
+                .filter(usuario -> usuario.getNombre().equals(nombre))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void editarUsuario(Usuario usuario) throws Exception{
+        Usuario usuarioBuscado = buscarUsuarioNombre(usuario.getNombre());
+        if (usuarioBuscado == null){
+            usuarioBuscado = buscarUsuarioTelefono(usuario.getTelefono());
+        }
+
+        if(usuarioBuscado!=null){
+            usuarioBuscado.setNombre(usuario.getNombre());
+            usuarioBuscado.setApellido(usuario.getApellido());
+            usuarioBuscado.setTelefono(usuario.getTelefono());
+        }else{
+            throw new Exception("No existe un usuario con el nombre dado");
+        }
+    }
+
+    public void eliminar(String nombre, String telefono) throws Exception{
+        Usuario usuarioBuscado = buscarUsuarioNombre(nombre);
+        if (usuarioBuscado == null){
+            usuarioBuscado = buscarUsuarioTelefono(telefono);
+        }
+
+        if(usuarioBuscado==null){
+            throw new Exception("No existe un usuario con el nombre dado");
+        }else{
+            listaUsuarios.remove(usuarioBuscado);
+        }
     }
 }
