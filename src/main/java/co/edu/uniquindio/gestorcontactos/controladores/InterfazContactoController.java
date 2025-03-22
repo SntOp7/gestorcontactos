916c3a17daa547e9a7ce46edf971fde742fbe01f
Sharif Giraldo Obando <sharifgiraldo23@gmail.com>
2025-03-22 18:26:57 -0500

    Create InterfazContactoController.java

---
+++ src/main/java/co/edu/uniquindio/gestorcontactos/controladores/InterfazContactoController.java
@@ -0,0 +1,42 @@
+package co.edu.uniquindio.gestorcontactos.controladores;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
+public class InterfazContactoController {
+
+
+        @FXML
+        private TextField buscarContactoTxt;
+
+        @FXML
+        private TableColumn<?, ?> tablaContacto;
+
+        @FXML
+        private ComboBox<?> filtrarBox;
+
+        @FXML
+        private ComboBox<?> opcionesBox;
+
+        @FXML
+        void buscarTxtAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void filtrarBoxAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void opcionesBoxAction(ActionEvent event) {
+
+        }
+
+    }
+
+}
