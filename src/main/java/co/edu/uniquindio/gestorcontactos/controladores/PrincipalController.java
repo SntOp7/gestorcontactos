c7b02456662d426abbceedd71a899d23b3104aa8
Sharif Giraldo Obando <sharifgiraldo23@gmail.com>
2025-03-22 21:23:03 -0500

    fxml  image changes

--- src/main/java/co/edu/uniquindio/gestorcontactos/controladores/PrincipalController.java
+++ src/main/java/co/edu/uniquindio/gestorcontactos/controladores/PrincipalController.java
@@ -8,10 +8,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -80,4 +82,8 @@ public class PrincipalController extends Controller implements Initializable {
         private void seleccionarUsuario() {
                 tblContactos.setOnMouseClicked(e -> setUsuarioSelected(tblContactos.getSelectionModel().getSelectedItem()));
         }
+
+
+
+
 }
