bdb7b99f0170dda6e35b9b0dbe8cdd28fb122b6f
Sharif Giraldo Obando <sharifgiraldo23@gmail.com>
2025-03-28 20:39:25 -0500

    interfaz y controller de contacto repetido

---
+++ src/main/java/co/edu/uniquindio/gestorcontactos/controladores/RepetidoController.java
@@ -0,0 +1,25 @@
+package co.edu.uniquindio.gestorcontactos.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RepetidoController {
+
+    @FXML
+    private Button cancelarButton;
+
+    @FXML
+    private Button reemplazarButton;
+
+    @FXML
+    void reemplazarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelarAction(ActionEvent event) {
+
+    }
+
+}
