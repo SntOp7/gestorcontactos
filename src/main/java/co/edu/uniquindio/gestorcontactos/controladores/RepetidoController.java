c036ff0a2d83aa6b22e5cae8bdf2ee3b9fceff97
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2025-03-28 22:26:21 -0500

    Repetido View 2

--- src/main/java/co/edu/uniquindio/gestorcontactos/controladores/RepetidoController.java
+++ src/main/java/co/edu/uniquindio/gestorcontactos/controladores/RepetidoController.java
@@ -1,16 +1,30 @@
 package co.edu.uniquindio.gestorcontactos.controladores;
 
+import co.edu.uniquindio.gestorcontactos.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import lombok.Setter;
 
-public class RepetidoController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RepetidoController extends Controller implements Initializable {
 
     @FXML
     private Button cancelarButton;
 
     @FXML
     private Button reemplazarButton;
+
+    @Setter
+    private App app;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
     @FXML
     void reemplazarAction(ActionEvent event) {
@@ -21,5 +35,4 @@ public class RepetidoController {
     void cancelarAction(ActionEvent event) {
 
     }
-
 }
